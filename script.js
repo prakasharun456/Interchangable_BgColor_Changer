528d00f61d3ebdc15dbd05f4c9e58b2a47a8280b
Arunprakash Venkatraju <145740589+prakasharun456@users.noreply.github.com>
2024-07-25 22:44:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const buttons=document.querySelectorAll(".btn")
+const body=document.body
+
+buttons.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+    color=btn.value;
+    changeBGColor(color)
+    })
+})
+
+function changeBGColor(color){
+    body.className="";
+    switch(color){
+        case "purple":
+            body.classList.add("purple");
+            break;
+        case "blue":
+            body.classList.add("blue");
+            break;
+        case "red":
+            body.classList.add("red");
+            break;
+        case "green":
+            body.classList.add("green");
+            break;
+        case "yellow":
+            body.classList.add("yellow");
+            break;
+        case "teal":
+            body.classList.add("teal");
+            break;
+        default:
+            break;
+    }
+}
